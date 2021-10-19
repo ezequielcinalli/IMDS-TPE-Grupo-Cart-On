e786328214281f35991868a69d9e68a07671cd50
ezequiel <ezequielcinalli96@gmail.com>
2021-10-19 20:52:06 -0300

    CCO-82 Added table accepted_material to script db

--- db/db.sql
+++ db/db.sql
@@ -1,1 +1,5 @@
 CREATE DATABASE imds_tpe;
+
+CREATE TABLE `imds_tpe`.`accepted_material` ( `id` INT NOT NULL AUTO_INCREMENT , `material` VARCHAR(50) NOT NULL , `deliveryMethod` VARCHAR(50) NOT NULL , `image` VARCHAR(50) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB; 
+
+
