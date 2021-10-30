3e3c4fc44bb21cf6b3d24e9a07531aa1e8f29963
ezequiel <ezequielcinalli96@gmail.com>
2021-10-30 19:42:33 -0300

    CCO-125 Added login routes and improved router

--- router.php
+++ router.php
@@ -24,42 +24,50 @@ if (!empty($_GET["action"])) {
 }
 
 $params = explode("/", $action);
-$controllerCitizen = new CitizenController();
 $controllerMain = new MainController();
+$controllerCitizen = new CitizenController();
 $controllerAcceptedMaterial = new AcceptedMaterialController();
 $controllerSecretary= new SecretaryController();
 
 // determina que camino seguir según la acción
 switch ($params[0]) {
-    case "home":
-        $controllerMain->showHome();
-        break;
-    case "materiales-aceptados":
-        $controllerAcceptedMaterial->showAcceptedMaterials();
-        break;
-    case "condiciones-entrega":
-        // el parametro [1] indica el id o nombre
-        if (isset($params[1])) {
-            $controllerAcceptedMaterial->showDeliveryConditions($params[1]);
-        } else {
-            $controllerCitizen->showError404();
-        }
-        break;
+  //CITIZEN CONTROLLER
     case "registrar-pedido-retiro":
         $controllerCitizen->showRegisterRetirementRequest();
         break;
     case "enviar-orden":
         $controllerCitizen->registerRetirementRequest();
+  //ACCEPTEDMATERIAL CONTROLLER
+    case "materiales-aceptados":
+        $controllerAcceptedMaterial->showAcceptedMaterials();
         break;
+    case "condiciones-entrega":
+        if (isset($params[1])) {
+            $controllerAcceptedMaterial->showDeliveryConditions($params[1]);
+        } else {
+            $controllerMain->showError404();
+        }
+        break;
+        break;
+  //SECRETARY CONTROLLER LOGIN
+    case 'login': 
+        $controllerSecretary->login();
+        break;
+    case 'checkLogin': 
+        $controllerSecretary->checkLogin();
+        break;
+  //SECRETARY CONTROLLER - ADMIN SECTION
     case 'admin-materiales': // sección ABM de materiales
         $controllerSecretary->showSecretaryMaterials();
         break;
-    //ABM materiales
-    //ADMIN
     case 'insertar-material': //form de admin. boton agregar
         $controllerSecretary->addAcceptedMaterial();
         break;
+  //MAIN CONTROLLER
+    case "home":
+        $controllerMain->showHome();
+        break;
     default:
-        $controllerCitizen->showError404();
+        $controllerMain->showError404();
         break;
 }
