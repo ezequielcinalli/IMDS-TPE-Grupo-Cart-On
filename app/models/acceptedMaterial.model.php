762e49097391309e269f7dbc0766a3fde146db41
Manuela Fernandez <manu.fer.0396@gmail.com>
2021-11-05 19:38:51 -0300

    CCO-142 Added model function to delete material's image on database

--- app/models/acceptedMaterial.model.php
+++ app/models/acceptedMaterial.model.php
@@ -98,5 +98,19 @@ class AcceptedMaterialModel{
          $query = $this->db->prepare($sql);
          $result= $query->execute($params); 
          return $result;
-     }
+    }
+
+    /**
+     * Elimina la imagen del material reciclable en DB con el id pasado por parametro
+     */
+    function deleteImg($type, $deliveryMethod,$id, $img){
+      if($img){  //si existe imagen
+        $img = null;
+        $sql = 'UPDATE accepted_material SET material= ?, deliveryMethod= ?, image= ? WHERE id = '.$id.' ';
+        $params = [$type, $deliveryMethod, $img];
+
+        $query =$this->db->prepare($sql);
+        return $query->execute($params);
+      }
+    }
 }
